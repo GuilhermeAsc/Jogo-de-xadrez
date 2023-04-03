6e250bc5069e6633fbcd25718cf5dc950f2abf86
GuilhermeAsc <99231465+GuilhermeAsc@users.noreply.github.com>
2023-04-03 18:54:23 -0300

    Implementing moves of rook

--- bin/boardgame/Position.java
+++ bin/boardgame/Position.java
@@ -21,8 +21,13 @@ public class Position{
     return column;
   }
 
-  public void setCollumn(int collumn) {
-    this.column = collumn;
+  public void setColumn(int column) {
+    this.column = column;
+  }
+
+  public void setValues(int row, int column){
+    this.row = row;
+    this.column = column;
   }
   @Override
   public String toString(){
