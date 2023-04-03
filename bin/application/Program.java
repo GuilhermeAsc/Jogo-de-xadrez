b734b8cf929f043064dbf4fb79f65b2ef4d966fa
GuilhermeAsc <99231465+GuilhermeAsc@users.noreply.github.com>
2023-04-03 17:12:19 -0300

    Clearing terminal

---
+++ bin/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    ChessMatch chessMatch = new ChessMatch();
+
+    while(true){
+      try{
+        UI.clearScreen();
+        UI.printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Source: ");
+        ChessPosition source = UI.readChessPosition(sc);
+        
+        System.out.println();
+        System.out.println("Target: ");
+        ChessPosition target = UI.readChessPosition(sc);
+  
+        ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+      } catch(ChessException e){
+        System.out.println(e.getMessage());
+        sc.nextLine();
+      }catch(InputMismatchException e){
+        System.out.println(e.getMessage());
+        sc.nextLine();
+      }      
+
+
+
+    }
+
+    
+    
+  }
+
+}
