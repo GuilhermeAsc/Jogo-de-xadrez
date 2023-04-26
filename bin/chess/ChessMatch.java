8c24676770bd2415f7dc6490fe0f4eb44232b73c
GuilhermeAsc <99231465+GuilhermeAsc@users.noreply.github.com>
2023-04-26 17:57:37 -0300

    Implementing Knight on board

--- bin/chess/ChessMatch.java
+++ bin/chess/ChessMatch.java
@@ -13,6 +13,7 @@ import chess.chessPieces.Rook;
 import chess.chessPieces.Bishop;
 import chess.chessPieces.King;
 import chess.chessPieces.Pawn;
+import chess.chessPieces.Knight;
 
 public class ChessMatch {
 
@@ -183,12 +184,13 @@ public class ChessMatch {
   private void initialSetup(){
     // WHITE TEAM
     placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-
+    placeNewPiece('b', 1, new Knight(board, Color.WHITE));
     placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('e', 1, new King(board, Color.WHITE));
-
+    
     placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+    placeNewPiece('g', 1, new Knight(board, Color.WHITE));
     placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
     placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
     placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -200,11 +202,12 @@ public class ChessMatch {
     
     // Black Team
     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-
+    placeNewPiece('b', 8, new Knight(board, Color.BLACK));
     placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
     placeNewPiece('e', 8, new King(board, Color.BLACK));
-
+    
     placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+    placeNewPiece('g', 8, new Knight(board, Color.BLACK));
     placeNewPiece('h', 8, new Rook(board, Color.BLACK));
     placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
     placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
